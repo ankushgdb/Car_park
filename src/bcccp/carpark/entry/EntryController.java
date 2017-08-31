cb011c05c9c27676674c080885681290edde6a66
Ankush Nagpal <nagpalgdb@gmail.com>
2017-08-31 14:00:40 +1000

    changes in entry conroller file

--- src/bcccp/carpark/entry/EntryController.java
+++ src/bcccp/carpark/entry/EntryController.java
@@ -1,19 +1,12 @@
 package bcccp.carpark.entry;
 
 import bcccp.carpark.Carpark;
+
 import bcccp.carpark.ICarSensor;
 import bcccp.carpark.ICarSensorResponder;
 import bcccp.carpark.ICarpark;
 import bcccp.carpark.ICarparkObserver;
 import bcccp.carpark.IGate;
-import bcccp.tickets.adhoc.AdhocTicket;
-import bcccp.tickets.adhoc.AdhocTicketDAO;
-import bcccp.tickets.adhoc.AdhocTicketFactory;
-import bcccp.tickets.adhoc.IAdhocTicket;
-import bcccp.tickets.adhoc.IAdhocTicketDAO;
-import bcccp.tickets.adhoc.IAdhocTicketFactory;
-import bcccp.tickets.season.ISeasonTicket;
-import bcccp.tickets.season.SeasonTicketDAO;
 
 public class EntryController 
 		implements ICarSensorResponder,
