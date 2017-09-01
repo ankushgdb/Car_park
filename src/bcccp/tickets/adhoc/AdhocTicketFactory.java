3ebd89ec3b1957740d1045ce54bda9e9d5d22dbe
hailey-2210 <haihado.91@gmail.com>
2017-09-01 11:04:54 +1000

    HD modified adhocTicket v.2.0: factory, naming of private vars

--- src/bcccp/tickets/adhoc/AdhocTicketFactory.java
+++ src/bcccp/tickets/adhoc/AdhocTicketFactory.java
@@ -1,19 +1,22 @@
-/*This class create a barcode based on date and time of entry i.e. System time
- * as it is assumed that one car enters at one time only 
- * carparkId and ticketNo are parsed as parameters
+/* This class create a barcode of ticket with carparkId and ticketNo are parsed as parameters
+ * The barcode is the form: “A” + hexstring representation of ticket number 
+ * + hextring representation of entry date and time.
  * 
  */
 
 package bcccp.tickets.adhoc;
 
+import java.util.Date;
 
 public class AdhocTicketFactory implements IAdhocTicketFactory {
 
 	@Override
 	public IAdhocTicket make(String carparkId, int ticketNo) {
 		// TODO Auto-generated method stub
-		String barcode = String.valueOf(System.currentTimeMillis());
+		Long entryDateTime = new Date().getTime();
+		String barcode = 'A' + Integer.toHexString(ticketNo) + Long.toHexString(entryDateTime);
 		IAdhocTicket adhocTicket = new AdhocTicket (carparkId, ticketNo, barcode);
+		adhocTicket.enter(entryDateTime); // record entry date & time
 		return adhocTicket;
 	}
 
