64e394ed4d8d0df538d4b0d70785464d4f61a606
baljnyam <baljnyam@gmail.com>
2017-09-18 12:02:04 +1000

    add log method

--- src/bcccp/carpark/exit/ExitController.java
+++ src/bcccp/carpark/exit/ExitController.java
@@ -58,6 +58,9 @@ public ExitController(Carpark carpark, IGate exitGate,
 	setState(STATE.IDLE);		
 }
 
+private void log(String message) {
+	System.out.println("ExitController : " + message);
+}
 
 /*
  * check car
