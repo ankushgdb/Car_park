3f40acddeeb5fc13e4919f6e80179f6dd31b485d
baljnyam <baljnyam@gmail.com>
2017-09-21 01:01:02 +1000

    fixed

--- src/bcccp/tickets/adhoc/AdhocTicket.java
+++ src/bcccp/tickets/adhoc/AdhocTicket.java
@@ -1,183 +1,227 @@
-/*
- * This method creates an adhocTicket object
- * with constructor of carparkId, ticketNo, barcode
- * ticketNo is incremented according to carpark
- * barcode consists of A + hexstring of ticketNo + hexstring of dateTime
- */
 package bcccp.tickets.adhoc;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
 import java.util.Date;
+import java.util.regex.Pattern;
+import javax.xml.bind.DatatypeConverter;
 
+/**
+ * A ticket for casual carpark users.
+ */
 public class AdhocTicket implements IAdhocTicket {
 
-	private String carparkId_;
-	private int ticketNo_;
-	private long entryDateTime_;
-	private long paidDateTime_;
-	private long exitDateTime_;
-	private float charge_;
-	private String barcode_;
-	private STATE state_;
 
-	/*
-	 * Create the state of ticket:
-	 * ISSUED: the ticket has been issued
-	 * CURRENT: when the ticket has been issued and car has entered, not yet paid
-	 * PAID: when the ticket has been paid
-	 * EXITED: when the car has exited
-	 */
-	private enum STATE { ISSUED, CURRENT, PAID, EXITED };
+    private String carparkId;
+    private int ticketNo;
+    private long entryDateTime;
+    private long paidDateTime;
+    private long exitDateTime;
+    private float charge;
+    private String barcode;
+    private STATE state;
+    private String regex = "\\d{8}";
+
+    private enum STATE {
+        ISSUED,
+        CURRENT,
+        PAID,
+        EXITED
+    }
+
+    /**
+     * A ticket for casual carpark clients.
+     *
+     * @param carparkId the carpark. Cannot be null or empty.
+     * @param ticketNo  the ticket number. Cannot be zero or negative.
+     * @param barcode   the string of values encoded by the barcode. Cannot be null or empty.
+     */
+    public AdhocTicket(String carparkId, int ticketNo, String barcode) {
+
+        if (isValue(carparkId) && isValidID(ticketNo) && isValue(barcode)) {
+
+            this.carparkId = carparkId;
+
+            this.ticketNo = ticketNo;
+
+            this.barcode = barcode;
+
+            barCodeInfo(barcode);
+
+            state = STATE.ISSUED;
+
+        } else {
+
+            throw new IllegalArgumentException(
+                    "Invalid Input: check that the arguments passed to the constructor " + "are valid.");
+        }
+    }
+
+    @Override
+    public int getTicketNo() {
+
+        return ticketNo;
+    }
+
+    @Override
+    public String getBarcode() {
+        return barcode;
+    }
+
+    @Override
+    public String getCarparkId() {
+        return carparkId;
+    }
+
+    @Override
+    public void enter(long dateTime) {
 
 
-	public AdhocTicket(String carparkId, int ticketNo, String barcode) {
-		//TDO Implement constructor
-		if(carparkId == null || barcode == null) {
-			throw new IllegalArgumentException("Parameters cannot be null");
-		}
-		if (ticketNo <=0) {
-			throw new IllegalArgumentException("Ticket number must be larger than 0");
-		}
+        entryDateTime = dateTime;
 
-		carparkId_ = carparkId;
-		ticketNo_ = ticketNo;
-		barcode_ = barcode;
-		state_ = STATE.ISSUED;
+        state = STATE.CURRENT;
+
+
+    }
+
+    @Override
+    public long getEntryDateTime() {
+
+        return entryDateTime;
+    }
+
+    @Override
+    public boolean isCurrent() {
+
+        return state == STATE.CURRENT;
+    }
+
+    @Override
+    public void pay(long dateTime, float charge) {
+
+        paidDateTime = dateTime;
+
+        this.charge = charge;
+
+        state = STATE.PAID;
+    }
+
+    @Override
+    public long getPaidDateTime() {
+
+        return paidDateTime;
+    }
+
+    @Override
+    public boolean isPaid() {
+
+        return state == STATE.PAID;
+    }
+
+    @Override
+    public float getCharge() {
+
+        return charge;
+    }
+
+    public String toString() {
+        Date entryDate = new Date(entryDateTime);
+        Date paidDate = new Date(paidDateTime);
+        Date exitDate = new Date(exitDateTime);
+
+        return "Carpark: " + carparkId + "\n"
+                + "Ticket No: " + ticketNo + "\n"
+                + "Entry Time: " + entryDate + "\n"
+                + "Paid Time: " + paidDate + "\n"
+                + "Exit Time: " + exitDate + "\n"
+                + "State: " + state + "\n"
+                + "Barcode: " + barcode;
+    }
+
+    @Override
+    public void exit(long dateTime) {
+
+        exitDateTime = dateTime;
+
+        state = STATE.EXITED;
+    }
+
+    @Override
+    public long getExitDateTime() {
+
+        return exitDateTime;
+    }
+
+    @Override
+    public boolean hasExited() {
+
+        return state == STATE.EXITED;
+    }
+
+    private Boolean isValue(String str) {
+
+        return (str != null && !str.isEmpty());
+    }
+
+    private Boolean isValidID(int id) {
+
+        return id > 0;
+    }
+
+    /**
+     * Description: - Method for conversion of hex encoded ticket details
+     *
+     * @param barcode
+     */
+    @SuppressWarnings("deprecation")
+	private void barCodeInfo(String barcode) {
+
+        // U+002D ‭ - unicode HYPHEN-MINUS
+        Pattern p = Pattern.compile("\u002D", Pattern.LITERAL);
+
+        String[] elements = p.split(barcode);
+
+        System.out.println("barcode:" + barcode);
+
+        if (elements[1].length() % 2 == 1) elements[1] = "0" + elements[1];
+
+        if (elements[2].length() % 2 == 1) elements[2] = "0" + elements[2];
+
+        System.out.println("Datehex:" + elements[2]);
+
+        ticketNo = Integer.parseInt(elements[1], 16);
+
+        System.out.println("Ticket Number: " + ticketNo);
+
+        byte[] entryDateBytes = DatatypeConverter.parseHexBinary(elements[2]);
+
+        String entryDate = null;
+        try {
+            entryDate = new String(entryDateBytes, "UTF-16");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("DateString:" + entryDate);
+
+        entryDateTime =
+                new Date(
+                        Integer.parseInt(entryDate.substring(4, 8)) - 1900,
+                        Integer.parseInt(entryDate.substring(2, 4)) - 1,
+                        Integer.parseInt(entryDate.substring(0, 2)),
+                        Integer.parseInt(entryDate.substring(8, 10)),
+                        Integer.parseInt(entryDate.substring(10, 12)),
+                        Integer.parseInt(entryDate.substring(12)))
+                        .getTime();
+    }
+
+	public String getRegex() {
+		return regex;
 	}
 
-
-	@Override
-	public int getTicketNo() {
-		// TODO Auto-generated method stub
-		return ticketNo_;
+	public void setRegex(String regex) {
+		this.regex = regex;
 	}
 
-
-	@Override
-	public String getBarcode() {
-		// TODO Auto-generated method stub
-		return barcode_;
-	}
-
-
-	@Override
-	public String getCarparkId() {
-		// TODO Auto-generated method stub
-		return carparkId_;
-	}
-
-
-	@Override
-	public void enter(long entryDateTime) {
-		// TODO Auto-generated method stub
-		if (entryDateTime <=0) {
-			throw new RuntimeException("Date and time can't be negative or 0");
-		}
-		switch (state_) {
-		case ISSUED:
-			entryDateTime_ = entryDateTime;
-			state_ = STATE.CURRENT;	
-			break;
-		default:
-			throw new RuntimeException("Can only enter if the car in ISSUED state");
-		}			
-	}
-
-
-	@Override
-	public long getEntryDateTime() {
-		// TODO Auto-generated method stub
-		return entryDateTime_;
-	}
-
-
-	/* 	Checks if the state of the ticket is current
-	 */
-	@Override
-	public boolean isCurrent() {
-		// TODO Auto-generated method stub
-		return state_ == STATE.CURRENT;
-	}
-
-
-	@Override
-	public void pay(long paidDateTime, float charge) {
-		// TODO Auto-generated method stub
-		paidDateTime_ = paidDateTime;
-		charge_ = charge;
-		state_ = STATE.PAID;
-		}
-
-
-
-	@Override
-	public long getPaidDateTime() {
-		// TODO Auto-generated method stub
-		return paidDateTime_;
-	}
-
-
-	@Override
-	public boolean isPaid() {
-		// TODO Auto-generated method stub
-		return state_ == STATE.PAID;
-	}
-
-
-	@Override
-	public float getCharge() {
-		// TODO Auto-generated method stub
-		return charge_;
-	}
-
-
-	@Override
-	public void exit(long exitDateTime) {
-		// TODO Auto-generated method stub
-		if (exitDateTime <= paidDateTime_) {
-			throw new RuntimeException("Time of exit must be after time of payment");
-		}
-		switch(state_) {
-		case PAID:
-			exitDateTime_ = exitDateTime;
-			state_ = STATE.EXITED;
-			break;
-		default:
-			throw new RuntimeException("Can only exit if current ticket has been paid");
-		}
-
-
-	}
-
-
-	@Override
-	public long getExitDateTime() {
-		// TODO Auto-generated method stub
-		return exitDateTime_;
-	}
-
-
-	@Override
-	public boolean hasExited() {
-		// TODO Auto-generated method stub
-		return state_ == STATE.EXITED;
-	}
-
-	public STATE getState() {
-		return state_;
-	}
-	
-	public String toString() {
-		Date entryDateTime = new Date(entryDateTime_);
-		Date paidDateTime = new Date(paidDateTime_);
-		Date exitDateTime = new Date(exitDateTime_);
-
-		return "Carpark    : " + carparkId_ + "\n" +
-		"Ticket No  : " + ticketNo_ + "\n" +
-		"Entry Time : " + entryDateTime + "\n" + 
-		"Paid Time  : " + paidDateTime + "\n" + 
-		"Exit Time  : " + exitDateTime + "\n" +
-		"State      : " + state_ + "\n" +
-		"Barcode    : " + barcode_;		
-	}
 
 }
