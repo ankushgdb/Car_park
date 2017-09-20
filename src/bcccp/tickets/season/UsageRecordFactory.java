c37c735deef4c038c2658f86b91096794144c0d9
baljnyam <baljnyam@gmail.com>
2017-09-21 01:09:07 +1000

    Fixed

--- src/bcccp/tickets/season/UsageRecordFactory.java
+++ src/bcccp/tickets/season/UsageRecordFactory.java
@@ -1,17 +1,10 @@
-/*
- * This class produce one usage each time for each ticket
- */
-
 package bcccp.tickets.season;
 
 public class UsageRecordFactory implements IUsageRecordFactory {
 
 	@Override
 	public IUsageRecord make(String ticketId, long startDateTime) {
-		// TODO Auto-generated method stub
-		IUsageRecord usageRecord = new UsageRecord(ticketId, startDateTime);
-		return usageRecord;
+		return new UsageRecord(ticketId,startDateTime);
 	}
-
-
+	
 }
