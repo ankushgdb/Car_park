406cd61a907ca0594969f67c8cdef94d0aeebdda
baljnyam <baljnyam@gmail.com>
2017-09-21 00:50:22 +1000

    Completely changed adhocTicketFactory

--- src/bcccp/tickets/adhoc/AdhocTicketFactory.java
+++ src/bcccp/tickets/adhoc/AdhocTicketFactory.java
@@ -1,30 +1,58 @@
-/* This class create a barcode of ticket with carparkId and ticketNo are parsed as parameters
- * The barcode is the form: “A” + hexstring representation of ticket number 
- * + hextring representation of entry date and time.
- * 
- */
-
 package bcccp.tickets.adhoc;
 
+import javax.xml.bind.DatatypeConverter;
+import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * A ticket dispensation utility class.
+ */
 public class AdhocTicketFactory implements IAdhocTicketFactory {
 
-	@Override
-	public IAdhocTicket make(String carparkId, int ticketNo) {
-		// TODO Auto-generated method stub
-		if (carparkId == null) {
-			throw new IllegalArgumentException("Carpark ID cannot be null");
-		}
-		if (ticketNo <=0) {
-			throw new RuntimeException("Ticket number must be a positive number");
-		}
-		// Long entryDateTime = new Date().getTime();
-		String barcode = 'A' + Integer.toHexString(ticketNo);
-		IAdhocTicket adhocTicket = new AdhocTicket (carparkId, ticketNo, barcode);
-		// adhocTicket.enter(entryDateTime); // record entry date & time
-		return adhocTicket;
-	}
+    @Override
+    public IAdhocTicket make(String carparkId, int ticketNo) {
 
+        return new AdhocTicket(carparkId, ticketNo, generateBarCode(ticketNo, entryDate()));
+    }
+
+    /**
+     * Description <br>
+     * -the ticket issued to each ordinary customer has a bar code on it. The barcode <br>
+     * has a number on it and the date (ddmmyyyy) and time (hhmmss) of entry to the car park. <br>
+     *
+     * @return String
+     */
+    public String entryDate() {
+
+        // Display a date in day, month, year format
+        DateFormat formatter = new SimpleDateFormat("ddMMyyyyhhmmss");
+
+        return formatter.format(new Date().getTime()); // the string that is encoded (to a bar code)
+    }
+
+    public String generateBarCode(int ticketNum, String entryDate) {
+
+        String prefix = "0041"; // hex representation of "A". Unicode: U+0041
+
+        String hexNum = Integer.toHexString(ticketNum);
+
+        String hexDate = null;
+        try {
+            hexDate = toHexadecimal(entryDate);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return prefix + "\u002D" + hexNum + "\u002D" + hexDate;
+    }
+
+    public static String toHexadecimal(String text) throws UnsupportedEncodingException {
+        byte[] myBytes = text.getBytes("UTF-16");
+
+        return DatatypeConverter.printHexBinary(myBytes);
+    }
 
 }
+
