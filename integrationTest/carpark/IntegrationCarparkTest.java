d7da6e929d8907a9965bcc2f2d61ffecbc046716
hailey-2210 <haihado.91@gmail.com>
2017-09-25 22:42:52 +1000

    minor fix

--- integrationTest/carpark/IntegrationCarparkTest.java
+++ integrationTest/carpark/IntegrationCarparkTest.java
@@ -19,392 +19,382 @@ import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
 
-public class IntegCarparkTest {
-
-  static IAdhocTicketDAO adhocTicketDAO;
-
-  static ISeasonTicketDAO seasonTicketDAO;
-
-  static Carpark cp;
-
-  static Carpark testItem;
-
-  static EntryController entryController;
-
-  static String DEFAULT_CARPARK = "Alphabet Street";
-
-  static int DEFAULT_CAPACITY = 3;
-
-  private Logger logger = Logger.getLogger("Unit testing for Carpark class.");
+public class IntegrationCarparkTest {
+	static IAdhocTicketDAO adhocTicketDAO;
+	static ISeasonTicketDAO seasonTicketDAO;
+	static Carpark cp;
+	static Carpark testItem;
+	static EntryController entryController;
+	static String DEFAULT_CARPARK = "Bathurt Chase";
+	static int DEFAULT_CAPACITY = 3;
+	private Logger logger = Logger.getLogger("Unit testing for Carpark class.");
 
 
-  @BeforeAll
-  static void before() {
+	@BeforeAll
+	static void before() {
+		adhocTicketDAO = spy(new AdhocTicketDAO(new AdhocTicketFactory()));
+		seasonTicketDAO = spy(new SeasonTicketDAO(new UsageRecordFactory()));
 
-    adhocTicketDAO = spy(new AdhocTicketDAO(new AdhocTicketFactory()));
+		testItem = new Carpark(DEFAULT_CARPARK, DEFAULT_CAPACITY, adhocTicketDAO, seasonTicketDAO);
 
-    seasonTicketDAO = spy(new SeasonTicketDAO(new UsageRecordFactory()));
+		entryController = mock(EntryController.class);
 
-    testItem = new Carpark(DEFAULT_CARPARK, DEFAULT_CAPACITY, adhocTicketDAO, seasonTicketDAO);
-
-    entryController = mock(EntryController.class);
-
-  }
+	}
 
 
-  @AfterEach
-  void after() {
+	@AfterEach
+	void after() {
 
 
-    adhocTicketDAO = spy(new AdhocTicketDAO(new AdhocTicketFactory()));
+		adhocTicketDAO = spy(new AdhocTicketDAO(new AdhocTicketFactory()));
 
-    seasonTicketDAO = spy(new SeasonTicketDAO(new UsageRecordFactory()));
+		seasonTicketDAO = spy(new SeasonTicketDAO(new UsageRecordFactory()));
 
-    testItem = new Carpark(DEFAULT_CARPARK, DEFAULT_CAPACITY, adhocTicketDAO, seasonTicketDAO);
-
-
-  }
+		testItem = new Carpark(DEFAULT_CARPARK, DEFAULT_CAPACITY, adhocTicketDAO, seasonTicketDAO);
 
 
-  /* NOTE: Unchecked exceptions do not need to be declared in a method or constructor's throws clause if they can be thrown
+	}
+
+
+	/* NOTE: Unchecked exceptions do not need to be declared in a method or constructor's throws clause if they can be thrown
   by the execution of the method. See: https://docs.oracle.com/javase/7/docs/api/java/lang/RuntimeException.html
   What is actually required by the following test is for the constructor to throw an illegalArgumentException under specified conditions
   (which extends RuntimeException). */
-  @Test
-  void isValidConstruct() {
+	@Test
+	void isValidConstruct() {
 
-    logger.log(Level.INFO, "Testing exception handling for carpark constructor...");
+		logger.log(Level.INFO, "Testing exception handling for carpark constructor...");
 
 
-    // invalid 'name' argument: null
-    try {
+		// invalid 'name' argument: null
+		try {
 
-      Carpark testItem = new Carpark(null, 3, adhocTicketDAO, seasonTicketDAO);
+			Carpark testItem = new Carpark(null, 3, adhocTicketDAO, seasonTicketDAO);
 
-      fail("Expected a RuntimeException to be thrown");
+			fail("Expected a RuntimeException to be thrown");
 
-    } catch (RuntimeException e) {
+		} catch (RuntimeException e) {
 
-      assertEquals("Invalid argument passed to Carpark constructor: carparkId is null",
-          e.getMessage());
-    }
+			assertEquals("Invalid argument passed to Carpark constructor: carparkId is null",
+					e.getMessage());
+		}
 
-    // invalid 'name' argument: empty
-    try {
+		// invalid 'name' argument: empty
+		try {
 
-      Carpark testitem = new Carpark("", 3, adhocTicketDAO, seasonTicketDAO);
+			Carpark testitem = new Carpark("", 3, adhocTicketDAO, seasonTicketDAO);
 
-      fail("Expected a RuntimeException to be thrown");
+			fail("Expected a RuntimeException to be thrown");
 
-    } catch (Exception e) {
+		} catch (Exception e) {
 
-      assertEquals("Invalid argument passed to Carpark constructor: carparkId is empty",
-          e.getMessage());
-    }
+			assertEquals("Invalid argument passed to Carpark constructor: carparkId is empty",
+					e.getMessage());
+		}
 
-    // invalid 'capacity' argument: empty
-    try {
+		// invalid 'capacity' argument: empty
+		try {
 
-      Carpark testItem = new Carpark("Bathurst Chase", 0, adhocTicketDAO, seasonTicketDAO);
+			Carpark testItem = new Carpark("Bathurst Chase", 0, adhocTicketDAO, seasonTicketDAO);
 
-      fail("Expected a RuntimeException to be thrown");
+			fail("Expected a RuntimeException to be thrown");
 
-    } catch (Exception e) {
+		} catch (Exception e) {
 
-      assertEquals("Invalid argument passed to Carpark constructor: capacity is zero or negative",
-          e.getMessage());
-    }
+			assertEquals("Invalid argument passed to Carpark constructor: capacity is zero or negative",
+					e.getMessage());
+		}
 
-    // invalid 'capacity' argument: negative
-    try {
+		// invalid 'capacity' argument: negative
+		try {
 
-      Carpark testItem = new Carpark("Bathurst Chase", -1, adhocTicketDAO, seasonTicketDAO);
+			Carpark testItem = new Carpark("Bathurst Chase", -1, adhocTicketDAO, seasonTicketDAO);
 
-      fail("Expected a RuntimeException to be thrown");
+			fail("Expected a RuntimeException to be thrown");
 
-    } catch (Exception e) {
+		} catch (Exception e) {
 
-      assertEquals("Invalid argument passed to Carpark constructor: capacity is zero or negative",
-          e.getMessage());
-    }
-  }
+			assertEquals("Invalid argument passed to Carpark constructor: capacity is zero or negative",
+					e.getMessage());
+		}
+	}
 
-  @Test
-  /** returns the carpark name */
-  void getName() {
+	@Test
+	/** returns the carpark name */
+	void getName() {
 
-    logger.log(Level.INFO, "Testing getName...");
+		logger.log(Level.INFO, "Testing getName...");
 
-    assertEquals("Alphabet Street", testItem.getName());
-  }
+		assertEquals("Bathurt Chase", testItem.getName());
+	}
 
-  @Test
-  /** returns a boolean indicating whether the carpark is full (ie no adhoc spaces available) */
-  void isFull() {
+	@Test
+	/** returns a boolean indicating whether the carpark is full (ie no adhoc spaces available) */
+	void isFull() {
 
-    logger.log(Level.INFO, "Testing isFull...");
-    //cars + 1
-    testItem.recordAdhocTicketEntry();
-    //cars + 1
-    testItem.recordAdhocTicketEntry();
-    //cars + 1
-    testItem.recordAdhocTicketEntry();
+		logger.log(Level.INFO, "Testing isFull...");
+		//cars + 1
+		testItem.recordAdhocTicketEntry();
+		//cars + 1
+		testItem.recordAdhocTicketEntry();
+		//cars + 1
+		testItem.recordAdhocTicketEntry();
 
-    assertEquals(true, testItem.isFull());
+		assertEquals(true, testItem.isFull());
 
-  }
+	}
 
-  @Test
-  /**
-   * if spaces for adhoc parking are available returns a valid new AdhocTicket throws a
-   * RuntimeException if called when carpark is full (ie no adhoc spaces available)
-   */
-  void issueAdhocTicket() {
+	@Test
+	/**
+	 * if spaces for adhoc parking are available returns a valid new AdhocTicket throws a
+	 * RuntimeException if called when carpark is full (ie no adhoc spaces available)
+	 */
+	void issueAdhocTicket() {
 
-    logger.log(Level.INFO, "Testing issueAdhocTicket...");
+		logger.log(Level.INFO, "Testing issueAdhocTicket...");
 
-    testItem.issueAdhocTicket();
+		testItem.issueAdhocTicket();
 
-    verify(adhocTicketDAO).createTicket("Alphabet Street");
+		verify(adhocTicketDAO).createTicket("Bathurt Chase");
 
-  }
+	}
 
-  @Test
-  /**
-   * registers observer as an entity to be notified through the notifyCarparkEvent method when the
-   * carpark is full and spaces become available
-   */
-  void register() {
+	@Test
+	/**
+	 * registers observer as an entity to be notified through the notifyCarparkEvent method when the
+	 * carpark is full and spaces become available
+	 */
+	void register() {
 
 
-    logger.log(Level.INFO, "EntryController added....");
+		logger.log(Level.INFO, "EntryController added....");
 
-    testItem.register(entryController);
+		testItem.register(entryController);
 
 
-  }
+	}
 
-  @Test
-  /** remove observer as an entity to be notified */
-  void deregister() {
+	@Test
+	/** remove observer as an entity to be notified */
+	void deregister() {
 
 
-    logger.log(Level.INFO, "EntryController removed....");
+		logger.log(Level.INFO, "EntryController removed....");
 
-    testItem.deregister(entryController);
+		testItem.deregister(entryController);
 
 
-  }
+	}
 
-  @Test
-  void recordAdhocTicketExit() {
+	@Test
+	void recordAdhocTicketExit() {
 
-    logger.log(Level.INFO, "Testing recordAdhocTicketExit...");
-    //cars + 1
-    testItem.recordAdhocTicketEntry();
-    //cars + 1
-    testItem.recordAdhocTicketEntry();
-    //cars + 1 = capacity = 3 = full
-    testItem.recordAdhocTicketEntry();
+		logger.log(Level.INFO, "Testing recordAdhocTicketExit...");
+		//cars + 1
+		testItem.recordAdhocTicketEntry();
+		//cars + 1
+		testItem.recordAdhocTicketEntry();
+		//cars + 1 = capacity = 3 = full
+		testItem.recordAdhocTicketEntry();
 
-    if (testItem.isFull())
-      logger.log(Level.INFO, "Carpark has reached capacity...");
-    //3 - 1 = 2  (capacity - 1)
-    testItem.recordAdhocTicketExit();
-    logger.log(Level.INFO, "Carpark has a space available...");
+		if (testItem.isFull())
+			logger.log(Level.INFO, "Carpark has reached capacity...");
+		//3 - 1 = 2  (capacity - 1)
+		testItem.recordAdhocTicketExit();
+		logger.log(Level.INFO, "Carpark has a space available...");
 
-    assertEquals(false, testItem.isFull());
+		assertEquals(false, testItem.isFull());
 
 
-  }
+	}
 
-  @Test
-  /**
-   * increments the number of adhoc carpark spaces in use. May cause the carpark to become full (ie
-   * all adhoc spaces filled)
-   */
-  void recordAdhocTicketEntry() {
+	@Test
+	/**
+	 * increments the number of adhoc carpark spaces in use. May cause the carpark to become full (ie
+	 * all adhoc spaces filled)
+	 */
+	void recordAdhocTicketEntry() {
 
-    logger.log(Level.INFO, "Testing recordAdhocTicketEntry...");
-    //cars + 1
-    testItem.recordAdhocTicketEntry();
-    //cars + 1
-    testItem.recordAdhocTicketEntry();
-    //car + 1
-    testItem.recordAdhocTicketEntry();
+		logger.log(Level.INFO, "Testing recordAdhocTicketEntry...");
+		//cars + 1
+		testItem.recordAdhocTicketEntry();
+		//cars + 1
+		testItem.recordAdhocTicketEntry();
+		//car + 1
+		testItem.recordAdhocTicketEntry();
 
-    assertEquals(true, testItem.isFull());
+		assertEquals(true, testItem.isFull());
 
-  }
+	}
 
-  @Test
-  /**
-   * returns the adhoc ticket identified by the barcode, returns null if the ticket does not exist,
-   * or is not current (ie not in use).
-   */
-  void getAdhocTicket() {
+	@Test
+	/**
+	 * returns the adhoc ticket identified by the barcode, returns null if the ticket does not exist,
+	 * or is not current (ie not in use).
+	 */
+	void getAdhocTicket() {
 
-    logger.log(Level.INFO, "Testing getAdhocTicket...");
+		logger.log(Level.INFO, "Testing getAdhocTicket...");
 
-    IAdhocTicket expected = testItem.issueAdhocTicket();
+		IAdhocTicket expected = testItem.issueAdhocTicket();
 
-    IAdhocTicket ticket = testItem.getAdhocTicket(expected.getBarcode());
+		IAdhocTicket ticket = testItem.getAdhocTicket(expected.getBarcode());
 
-    // This test is failing when only one ticket has been issued.
-    assertEquals(expected.getEntryDateTime(), ticket.getEntryDateTime());
+		// This test is failing when only one ticket has been issued.
+		assertEquals(expected.getEntryDateTime(), ticket.getEntryDateTime());
 
 
-  }
+	}
 
-  @Test
-  /**
-   * registers a season ticket with the carpark so that the season ticket may be used to access the
-   * carpark.
-   *
-   * Note this test will fail because there is no usage record - requires ExitController.ticketInserted()
-   * to call SeasonTicketDAO.recordTicketEntry().
-   */
-  void registerSeasonTicket() {
+	@Test
+	/**
+	 * registers a season ticket with the carpark so that the season ticket may be used to access the
+	 * carpark.
+	 *
+	 * Note this test will fail because there is no usage record - requires ExitController.ticketInserted()
+	 * to call SeasonTicketDAO.recordTicketEntry().
+	 */
+	void registerSeasonTicket() {
 
-    logger.log(Level.INFO, "Testing registerSeasonTicket...");
+		logger.log(Level.INFO, "Testing registerSeasonTicket...");
 
-    ISeasonTicket tktA = mock(SeasonTicket.class);
+		ISeasonTicket tktA = mock(SeasonTicket.class);
 
-    doReturn("S2222").when(tktA).getId();
+		doReturn("S2222").when(tktA).getId();
 
-    doReturn("Alphabet Street").when(tktA).getCarparkId();
+		doReturn("Bathurt Chase").when(tktA).getCarparkId();
 
-    testItem.registerSeasonTicket(tktA);
+		testItem.registerSeasonTicket(tktA);
 
-    verify(seasonTicketDAO).registerTicket(tktA);
+		verify(seasonTicketDAO).registerTicket(tktA);
 
-  }
+	}
 
-  /**
-   * Throws a RuntimeException if the carpark the season ticket is associated with is not the
-   * same as the carpark name.
-   */
-  @Test
-  void registerSeasonTicketRuntimeExceptionTest() {
+	/**
+	 * Throws a RuntimeException if the carpark the season ticket is associated with is not the
+	 * same as the carpark name.
+	 */
+	@Test
+	void registerSeasonTicketRuntimeExceptionTest() {
 
-    try {
+		try {
 
-      testItem.registerSeasonTicket(new SeasonTicket("S9999", "Wrong Name", 0L, 0L));
+			testItem.registerSeasonTicket(new SeasonTicket("S9999", "Wrong Name", 0L, 0L));
 
-      fail("Expected a RuntimeException to be thrown");
+			fail("Expected a RuntimeException to be thrown");
 
-    } catch (Exception e) {
+		} catch (Exception e) {
 
-      assertEquals(
-          "SeasonTicket in registerSeasonTicket has invalid CarparkId: Wrong Name, should be CarparkId: Alphabet Street",
-          e.getMessage());
-    }
+			assertEquals(
+					"SeasonTicket in registerSeasonTicket has invalid CarparkId: Wrong Name, should be CarparkId: Bathurt Chase",
+					e.getMessage());
+		}
 
-  }
+	}
 
 
-  @Test
-  /**
-   * deregisters the season ticket so that the season ticket may no longer be used to access the
-   * carpark
-   */
-  void deregisterSeasonTicket() {
+	@Test
+	/**
+	 * deregisters the season ticket so that the season ticket may no longer be used to access the
+	 * carpark
+	 */
+	void deregisterSeasonTicket() {
 
 
-    logger.log(Level.INFO, "Testing deregisterSeasonTicket...");
+		logger.log(Level.INFO, "Testing deregisterSeasonTicket...");
 
-    ISeasonTicket tkt = mock(SeasonTicket.class);
+		ISeasonTicket tkt = mock(SeasonTicket.class);
 
-    when(tkt.getId()).thenReturn("S2222");
+		when(tkt.getId()).thenReturn("S2222");
 
-    when(tkt.getCarparkId()).thenReturn("Alphabet Street");
+		when(tkt.getCarparkId()).thenReturn("Bathurt Chase");
 
-    testItem.registerSeasonTicket(tkt);
+		testItem.registerSeasonTicket(tkt);
 
-    testItem.deregisterSeasonTicket(tkt);
+		testItem.deregisterSeasonTicket(tkt);
 
-    verify(seasonTicketDAO).deregisterTicket(tkt);
+		verify(seasonTicketDAO).deregisterTicket(tkt);
 
-  }
+	}
 
-  @Test
-  void isSeasonTicketValid() {
+	@Test
+	void isSeasonTicketValid() {
 
-    //not tested (valid if other methods are tested and passing)
-  }
+		//not tested (valid if other methods are tested and passing)
+	}
 
 
 
-  /**
-   * causes a new usage record to be created and associated with a season ticket.
-   */
-  @Test
-  void recordSeasonTicketEntry() {
+	/**
+	 * causes a new usage record to be created and associated with a season ticket.
+	 */
+	@Test
+	void recordSeasonTicketEntry() {
 
-    logger.log(Level.INFO, "Testing recordSeasonTicketEntry...");
+		logger.log(Level.INFO, "Testing recordSeasonTicketEntry...");
 
-    ISeasonTicket tkt = mock(SeasonTicket.class);
+		ISeasonTicket tkt = mock(SeasonTicket.class);
 
-    when(tkt.getId()).thenReturn("S2222");
+		when(tkt.getId()).thenReturn("S2222");
 
-    when(tkt.getCarparkId()).thenReturn("Alphabet Street");
+		when(tkt.getCarparkId()).thenReturn("Bathurt Chase");
 
-    testItem.registerSeasonTicket(tkt);
+		testItem.registerSeasonTicket(tkt);
 
-    testItem.recordSeasonTicketEntry(tkt.getId());
+		testItem.recordSeasonTicketEntry(tkt.getId());
 
-    verify(seasonTicketDAO).recordTicketEntry("S2222");
+		verify(seasonTicketDAO).recordTicketEntry("S2222");
 
-  }
+	}
 
 
-  /**
-   * Throws a
-   * RuntimeException if the season ticket associated with ticketId does not exist, or is currently
-   * in use
-   */
-  @Test
-  void recordSeasonTicketEntryRuntimeExceptionTest() {
+	/**
+	 * Throws a
+	 * RuntimeException if the season ticket associated with ticketId does not exist, or is currently
+	 * in use
+	 */
+	@Test
+	void recordSeasonTicketEntryRuntimeExceptionTest() {
 
-    try {
-      //no ticket has the following id:
-      testItem.recordSeasonTicketEntry("badId");
+		try {
+			//no ticket has the following id:
+			testItem.recordSeasonTicketEntry("badId");
 
-      fail("Expected a RuntimeException to be thrown");
+			fail("Expected a RuntimeException to be thrown");
 
-    } catch (Exception e) {
+		} catch (Exception e) {
 
-      assertEquals("Runtime Exception: No corresponding ticket.", e.getMessage());
-    }
+			assertEquals("Runtime Exception: No corresponding ticket.", e.getMessage());
+		}
 
-    /** todo: test for exception thrown if id of car exists and is 'in use' */
-  }
+		/** todo: test for exception thrown if id of car exists and is 'in use' */
+	}
 
-  @Test
-  /**
-   * causes the current usage record of the season ticket associated with ticketID to be finalized.
-   * throws throws a RuntimeException if the season ticket associated with ticketId does not exist,
-   * or is not currently in use
-   */
-  void recordSeasonTicketExit() {
+	@Test
+	/**
+	 * causes the current usage record of the season ticket associated with ticketID to be finalized.
+	 * throws throws a RuntimeException if the season ticket associated with ticketId does not exist,
+	 * or is not currently in use
+	 */
+	void recordSeasonTicketExit() {
 
 
-    logger.log(Level.INFO, "Testing recordSeasonTicketExit...");
+		logger.log(Level.INFO, "Testing recordSeasonTicketExit...");
 
-    ISeasonTicket tkt = mock(SeasonTicket.class);
+		ISeasonTicket tkt = mock(SeasonTicket.class);
 
-    when(tkt.getId()).thenReturn("S2222");
+		when(tkt.getId()).thenReturn("S2222");
 
-    when(tkt.getCarparkId()).thenReturn("Alphabet Street");
+		when(tkt.getCarparkId()).thenReturn("Bathurt Chase");
 
-    testItem.registerSeasonTicket(tkt);
+		testItem.registerSeasonTicket(tkt);
 
-    testItem.recordSeasonTicketEntry(tkt.getId());
+		testItem.recordSeasonTicketEntry(tkt.getId());
 
-    testItem.recordSeasonTicketExit(tkt.getId());
+		testItem.recordSeasonTicketExit(tkt.getId());
 
-    verify(seasonTicketDAO).recordTicketExit(tkt.getId());
+		verify(seasonTicketDAO).recordTicketExit(tkt.getId());
 
-  }
+	}
 
 }
